e9087a26ff6c008b114fa40bdca04179c4a3fc22
heshandahao0703 <never_ove5@sina.com>
2018-06-29 12:27:43 +0800

    提交文件

---
+++ pymyself/test/test_cfg.py
@@ -0,0 +1,16 @@
+#_*_coding:utf-8 _*_
+__author__ = 'yuanyuan'
+__date__ = '2018/6/26 下午4:47'
+import pytest
+from pithy import Config
+
+class TestA:
+
+    def test_json_cfg(self):
+        cfg= Config('cfg.json')
+        assert cfg['a'] == 1
+
+
+    def test_yaml(self):
+        cfg= Config('cfg.yaml')
+        assert cfg['user'] == 'admi'
